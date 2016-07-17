70d1446abe1cb73b1032be299a78be933ef23552
FreeID <shuckir@gmail.com>
2016-07-17 12:59:29 +0400

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '75cb1b7b8006f9c34fb8960d469405eabdaac0e6c27f040112fc06a5c5bdb7091b8d935bc27e376bc20d7507d123b718e3ac7285d3d5c74457b6a511d5c58d82'
